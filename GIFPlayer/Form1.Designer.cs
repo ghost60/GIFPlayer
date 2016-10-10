b0d5680b83370a343b7ea8821c48b91d9c6121a6
ghost60 <570761003@qq.com>
2016-10-10 21:25:12 +0800

    add

---
+++ GIFPlayer/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GIFPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.giflist = new System.Windows.Forms.ListBox();
+            this.openfile = new System.Windows.Forms.Button();
+            this.openfiles = new System.Windows.Forms.Button();
+            this.play = new System.Windows.Forms.Button();
+            this.isLoop = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // giflist
+            // 
+            this.giflist.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.giflist.FormattingEnabled = true;
+            this.giflist.ItemHeight = 12;
+            this.giflist.Location = new System.Drawing.Point(0, 0);
+            this.giflist.Name = "giflist";
+            this.giflist.Size = new System.Drawing.Size(214, 652);
+            this.giflist.TabIndex = 1;
+            this.giflist.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.giflist_MouseDoubleClick);
+            // 
+            // openfile
+            // 
+            this.openfile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.openfile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openfile.Location = new System.Drawing.Point(57, 688);
+            this.openfile.Name = "openfile";
+            this.openfile.Size = new System.Drawing.Size(74, 23);
+            this.openfile.TabIndex = 2;
+            this.openfile.Text = "打开文件";
+            this.openfile.UseVisualStyleBackColor = true;
+            this.openfile.Click += new System.EventHandler(this.openfile_Click);
+            // 
+            // openfiles
+            // 
+            this.openfiles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.openfiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openfiles.Location = new System.Drawing.Point(134, 688);
+            this.openfiles.Name = "openfiles";
+            this.openfiles.Size = new System.Drawing.Size(76, 23);
+            this.openfiles.TabIndex = 2;
+            this.openfiles.Text = "打开文件夹";
+            this.openfiles.UseVisualStyleBackColor = true;
+            this.openfiles.Click += new System.EventHandler(this.openfiles_Click);
+            // 
+            // play
+            // 
+            this.play.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.play.Location = new System.Drawing.Point(6, 688);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(48, 23);
+            this.play.TabIndex = 3;
+            this.play.Text = "播放";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // isLoop
+            // 
+            this.isLoop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.isLoop.AutoSize = true;
+            this.isLoop.Location = new System.Drawing.Point(16, 666);
+            this.isLoop.Name = "isLoop";
+            this.isLoop.Size = new System.Drawing.Size(72, 16);
+            this.isLoop.TabIndex = 4;
+            this.isLoop.Text = "循环目录";
+            this.isLoop.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.giflist);
+            this.panel1.Controls.Add(this.isLoop);
+            this.panel1.Controls.Add(this.openfile);
+            this.panel1.Controls.Add(this.openfiles);
+            this.panel1.Controls.Add(this.play);
+            this.panel1.Location = new System.Drawing.Point(984, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(214, 716);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(1197, 715);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox giflist;
+        private System.Windows.Forms.Button openfile;
+        private System.Windows.Forms.Button openfiles;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.CheckBox isLoop;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GIFPlayer/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace GIFPlayer
+{
+    public partial class Form1 : Form
+    {
+        private Image nowgif = null;
+        private EventHandler m_evthdlAnimator = null;
+        private int count = 0;
+        private int nowcount = 0;
+        private int playno = 0;
+        private List<Image> playerlist = null;
+        public Form1()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            m_evthdlAnimator = new EventHandler(OnImageAnimate);
+            Debug.Assert(m_evthdlAnimator != null);
+        }
+         protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (nowgif != null)
+            {
+                UpdateImage();
+                int x = 0,y=0;
+                int height = this.Height;
+                int width =(int)( (nowgif.Width / (double)nowgif.Height) * height);
+                //if (nowgif.Width < this.Width / 2 && nowgif.Height < this.Height / 2)
+                //{
+                //    width = 2 * nowgif.Width;
+                //    height = 2 * nowgif.Height;
+                //}
+                x = (this.Width - width) / 2;
+                //y = (this.Height - 20 - height) / 2;
+                e.Graphics.DrawImage(nowgif, new Rectangle(x, y, width, height));
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (nowgif != null)
+            {
+                StopAnimate();
+                nowgif = null;
+            }
+        }
+        private void BeginAnimate()
+        {
+            if (nowgif == null)
+                return;
+
+            if (ImageAnimator.CanAnimate(nowgif))
+               {
+                   ImageAnimator.Animate(nowgif, m_evthdlAnimator);
+               }     
+        }
+
+        private void StopAnimate()
+        {
+            if (nowgif == null)
+                return;
+
+            if (ImageAnimator.CanAnimate(nowgif))
+            {
+                ImageAnimator.StopAnimate(nowgif, m_evthdlAnimator);
+            }
+        }
+
+        private void UpdateImage()
+        {
+            if (nowgif == null)
+                return;
+
+            FrameDimension fd = new FrameDimension(nowgif.FrameDimensionsList[0]);
+            count = nowgif.GetFrameCount(fd);
+
+            if (ImageAnimator.CanAnimate(nowgif))
+            {
+                ImageAnimator.UpdateFrames(nowgif);
+            }
+            if (isLoop.Checked)
+            {
+                nowcount++;
+                if (nowcount >= count)
+                {
+                    nowcount = 0;
+                    playno++;
+                    if (playno >= playerlist.Count)
+                    {
+                        playno = 0;
+                    }
+                    StopAnimate();
+                    nowgif = playerlist[playno];
+                    BeginAnimate();
+                }
+            }
+        }
+        private void OnImageAnimate(Object sender,EventArgs e)
+        {
+            this.Invalidate(); 
+        }
+
+        private void openfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "GIF文件(*.gif)|*.gif";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string FileName = dialog.FileName;
+                giflist.Items.Add(FileName);
+            }
+        }
+
+        private void openfiles_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            if (folderDlg.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo TheFolder = new DirectoryInfo(folderDlg.SelectedPath);
+                foreach (FileInfo NextFile in TheFolder.GetFiles())
+                {
+                    if (NextFile.Extension==".gif")
+                    {
+                        giflist.Items.Add(NextFile.FullName);
+                    }
+                }                   
+            }
+        }
+
+        private void giflist_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (giflist.SelectedItems.Count>0)
+            {
+                playerlist = new List<Image>();
+                playerlist.Add(Image.FromFile(giflist.SelectedItems[0].ToString()));
+                nowgif = playerlist[0];
+                BeginAnimate();
+                Button btn = (Button)sender;
+                btn.Text = "暂停";
+            }
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text == "播放")
+            {
+                playerlist = new List<Image>();
+                if (giflist.Items.Count > 0)
+                {
+                    foreach (var item in giflist.Items)
+                    {
+                        Image gif = Image.FromFile(item.ToString());
+                        playerlist.Add(gif);
+                    }
+                }
+                nowgif = playerlist[0];
+                playno = 0;
+                BeginAnimate();
+                btn.Text = "暂停";
+            }
+            else
+            {
+                StopAnimate();
+                btn.Text = "播放";
+            }
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (this.panel1.Visible)
+            {
+                this.panel1.Visible = false;
+            }
+            else
+            {
+                this.panel1.Visible = true;
+            }
+        }
+    }
+}
